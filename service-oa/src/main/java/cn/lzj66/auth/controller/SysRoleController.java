7cc08654d822295c6ef7d6ee5b39d6d8095b0fda
LiuZhaoJun <1811176067@qq.com>
2024-06-03 16:54:32 +0800

    add swagger2

--- service-oa/src/main/java/cn/lzj66/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/cn/lzj66/auth/controller/SysRoleController.java
@@ -3,6 +3,8 @@ package cn.lzj66.auth.controller;
 import cn.lzj66.auth.service.SysRoleService;
 import cn.lzj66.entity.system.SysRole;
 import cn.lzj66.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +20,14 @@ import java.util.List;
  * @Author 工学院-liuzhaojun
  * @Create 2024/6/3 15:30
  */
+@Api(tags = "角色管理")
 @RestController //@Controller+@ResponseBody
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
     @Autowired
     private SysRoleService sysRoleService;
 
+    @ApiOperation(value = "获取全部角色列表")
     @GetMapping("findAll")
     public Result<List<SysRole>> findAll() {
         List<SysRole> roleList = sysRoleService.list();
